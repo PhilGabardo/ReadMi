4e06b47431c320f5c12089561b367f2daaa656b0
PhilGabardo <gabardpj@mcmaster.ca>
2019-05-12 14:20:46 -0700

    print song names

--- web/index.php
+++ web/index.php
@@ -19,7 +19,7 @@ $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider
 	)
 );
 $app->get('/db/', function() use($app) {
-	$st = $app['pdo']->prepare('SELECT name FROM test_table');
+	$st = $app['pdo']->prepare('SELECT name FROM songs');
 	$st->execute();
 
 	$names = array();
