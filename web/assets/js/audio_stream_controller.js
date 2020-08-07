c8398202519a1636ac97faf194a386b68aaf6b0e
PhilGabardo <gabardpj@mcmaster.ca>
2020-08-06 19:55:04 -0700

    simpler audio

--- web/assets/js/audio_stream_controller.js
+++ web/assets/js/audio_stream_controller.js
@@ -13,18 +13,11 @@ export default class AudioStreamController {
 		this.userMediaPromise.then(
 			function(stream) {
 				let source = that.audioContext.createMediaStreamSource(stream);
-				console.log(source);
-				let processor = that.audioContext.createScriptProcessor(4096, 1, 1);
-				console.log(processor)
 				that.analyser = that.audioContext.createAnalyser();
-				console.log(that.analyser)
 				that.analyser.fftSize = 2048;
 				that.buffer = new Uint8Array(that.analyser.fftSize);
-
-				source.connect(processor);
 				source.connect(that.analyser);
-				console.log(that.audioContext.destination);
-				processor.connect(that.audioContext.destination);
+				console.log("test");
 			}
 		)
 	}
