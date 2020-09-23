0e9d6363d2293ef97132066517f5f6841b92aad9
PhilGabardo <gabardpj@mcmaster.ca>
2020-09-23 08:12:23 -0700

    swal for feedback, styling fixes

--- web/assets/js/feedback.js
+++ web/assets/js/feedback.js
@@ -2,36 +2,43 @@ import swal from 'sweetalert';
 
 var feedback_button = document.getElementById('popup');
 feedback_button.onclick = function() {
-	var e = document.getElementById('feedback-main');
-
-	if(e.style.display == 'block')
-		e.style.display = 'none';
-	else
-		e.style.display = 'block';
-}
-
-var feedback_form = document.getElementById('feedback-form1');
-feedback_form.onsubmit = function(e) {
-	var button = document.getElementById('feedback-button-blue');
-	e.preventDefault();
-	let comment = escape(document.getElementById('feedback-comment').value);
-	fetch('/', {
-		method: "POST",
-		headers: new Headers({
-			'Content-Type': 'application/x-www-form-urlencoded', // <-- Specifying the Content-Type
-		}),
-		body: "action_type=feedback&comment=" + comment
-	}).then(function(response) {
-		return response.json();
-	}).then(function(responseJson) {
-		var success = responseJson.success;
-		var e = document.getElementById('feedback-main');
-		e.style.display = 'none';
-		if (success) {
-			swal("Feedback sent!");
-		} else {
-			swal("There was a problem sending your feedback, please try again.");
-		}
-	});
+	swal({
+		text: 'Enter your feedback.',
+		content: "input",
+		button: {
+			text: "Send!",
+			closeModal: false,
+		},
+	})
+		.then(feedback => {
+			if (!feedback) throw null;
+			return fetch('/', {
+				method: "POST",
+				headers: new Headers({
+					'Content-Type': 'application/x-www-form-urlencoded', // <-- Specifying the Content-Type
+				}),
+				body: "action_type=feedback&comment=" + escape(feedback)
+			});
+		})
+		.then(results => {
+			return results.json();
+		})
+		.then(responseJson => {
+			console.log(responseJson)
+			var success = responseJson.success;
+			if (success) {
+				swal("Feedback sent!");
+			} else {
+				swal("There was a problem sending your feedback, please try again.");
+			}
+		})
+		.catch(err => {
+			if (err) {
+				swal("There was a problem sending your feedback, please try again.");
+			} else {
+				swal.stopLoading();
+				swal.close();
+			}
+		});
 }
 
