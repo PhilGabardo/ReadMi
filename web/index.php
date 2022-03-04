51748e8bf3d4cb76f125f7fe30929e4798b57aa8
Philip Gabardo <philipgabardo@Philips-MacBook-Pro.local>
2022-03-03 18:25:25 -0800

    remove jeopardy endpoint

--- web/index.php
+++ web/index.php
@@ -87,59 +87,6 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-$app->get('/jeopardy', function(Request $request) use($app) {
-	if (($_SERVER['HTTP_HOST'] !== 'localhost:8080')) {
-		return '';
-	}
-	$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-		array(
-			'pdo.server' => array(
-				'driver'   => 'pgsql',
-				'user' => 'philipgabardo',
-				'password' => 'ninetythree',
-				'host' => '127.0.0.1',
-				'port' => 5432,
-				'dbname' => 'philipgabardo'
-			)
-		)
-	);
-	$categories = [];
-	for ($i = 0; $i < 6; $i++) {
-		$category_offset = random_int(0, 39659);
-		$st = $app['pdo']->prepare("SELECT distinct(category) FROM jeopardy_questions OFFSET $category_offset LIMIT 1");
-		$st->execute();
-		$categories[] = $st->fetch(PDO::FETCH_ASSOC)['category'];
-	}
-	$questions_by_category = [];
-	foreach ($categories as $category) {
-		$escaped_category = $app['pdo']->quote($category);
-		$st = $app['pdo']->prepare("SELECT year, question, answer FROM jeopardy_questions where category = $escaped_category");
-		$st->execute();
-		$question_candidates = $st->fetchAll(PDO::FETCH_ASSOC);
-		shuffle($question_candidates);
-		$questions_by_category[$category] = array_slice($question_candidates, 0, 6);
-	}
-	$daily_double_row = random_int(0, 4);
-	$daily_double_col = random_int(0, 5);
-	$questions_by_category[$categories[$daily_double_col]][$daily_double_row]['double'] = true;
-	$clues = [];
-	$is_double = (bool)$request->get('double');
-	$multiplier = $is_double ? 400 : 200;
-	for ($row = 0; $row < 5; $row++) {
-		$clues[$row + 1] = [];
-		foreach ($categories as $category) {
-			$questions_by_category[$category][$row]['double'] = $questions_by_category[$category][$row]['double'] ?? false;
-			$questions_by_category[$category][$row]['question'] = preg_replace('/^\(.*\)/', '', $questions_by_category[$category][$row]['question']);
-			$questions_by_category[$category][$row]['question'] = str_replace(' & ', ' and ', $questions_by_category[$category][$row]['question']);
-			$clues[$row + 1][] = $questions_by_category[$category][$row] + ['amount' => (($row + 1) * $multiplier)];
-		}
-	}
-	return $app['twig']->render('jeopardy.html', [
-		'categories' => $categories,
-		'clues' => $clues,
-	]);
-});
-
 $app->get('/password_reset', function(Request $request) use($app) {
 	return \Actions\DisplayPasswordResetAction::execute($app, $request);
 });
